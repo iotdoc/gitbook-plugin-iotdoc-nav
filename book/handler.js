89f12f35f9e76dd67776b84837fd0a01300e82c8
maomao.cai <maomao.cai@horizon.ai>
2019-06-18 11:43:17 +0800

    change http

--- book/handler.js
+++ book/handler.js
@@ -6,7 +6,7 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
 
     gitbook.events.bind("page.change", function(e) {
       function createHeader() {
-        return '<div class="cocos-navbar navbar-collapse" id="navbar" role="navigation"> <ul class="nav navbar-nav"> <li><a href="https://www.horizon.ai/" class="btn" target="_blank"> <img src="../resources/logo.png"  height="30" /></a></li> <li> <a href="#" class="btn autohide"><i class="fa fa-book"></i>开发文档</a> <ul class="hovershow"><li><a href="https://iotdoc.horizon.ai/busiopenapi/" class="btn">Open Api</a></li> <li><a href="#" class="btn">其他文档</a></li></ul> </li><li><a href="https://github.com/iotdoc/busiSDK/tree/master/auth" class="btn autohide"><i class="fa fa-link"></i>鉴权SDK</a></li><li><a href="http://localhost/iotdoc.zip" class="btn autohide"><i class="fa fa-download"></i>下载文档</a></li></ul></div>';
+        return '<div class="cocos-navbar navbar-collapse" id="navbar" role="navigation"> <ul class="nav navbar-nav"> <li><a href="https://www.horizon.ai/" class="btn" target="_blank"> <img src="../resources/logo.png"  height="30" /></a></li> <li> <a href="#" class="btn autohide"><i class="fa fa-book"></i>开发文档</a> <ul class="hovershow"><li><a href="http://10.10.108.168:4000/" class="btn">Open Api</a></li> <li><a href="#" class="btn">其他文档</a></li></ul> </li><li><a href="https://github.com/iotdoc/busiSDK/tree/master/auth" class="btn autohide"><i class="fa fa-link"></i>鉴权SDK</a></li><li><a href="http://localhost:8080/iotdoc.zip" class="btn autohide"><i class="fa fa-download"></i>下载文档</a></li></ul></div>';
       }
 	  
       // Get configuration.
